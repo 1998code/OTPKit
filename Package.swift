9ad4e82b773c6eefa11272de6dae4cf9ecab93ef
MingSwivel <54872601+1998code@users.noreply.github.com>
2022-01-06 00:03:00 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/norio-nomura/Base32.git", from: "0.8.0"),
-        .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", from: "4.2.0"),
+        .package(url: "https://github.com/norio-nomura/Base32.git", from: "0.9.0"),
+        .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", from: "4.2.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
